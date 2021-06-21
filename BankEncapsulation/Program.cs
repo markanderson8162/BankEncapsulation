1d6d3dbd374d41ab5ba315aaff83b767ca87b644
Mark Anderson <markanderson8162@gmail.com>
2021-06-21 13:34:39 -0500

    completed Encapsulation exercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BankAccount account = new BankAccount();
+			Console.WriteLine("How much money do you want to deposit?");
+			double deposit = double.Parse(Console.ReadLine());
+			account.Deposit(deposit);
+			double readBalance = account.GetBalance();
+			Console.WriteLine($"You deposited {readBalance} into your account.");
+
+			
+		}
+	}
+}
